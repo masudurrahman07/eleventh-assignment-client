29b4878617f8b5bf222e76a4a8c5b75fccf76acb
Masudur Rahman <rahmanmasudur57@gmail.com>
2025-12-05 21:50:35 +0600

    added firebase config

---
+++ src/auth/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrqv6Rq0FmUh7NHtHgChtOIvVpE0d1muw",
+  authDomain: "elventh-assignment.firebaseapp.com",
+  projectId: "elventh-assignment",
+  storageBucket: "elventh-assignment.firebasestorage.app",
+  messagingSenderId: "713344693424",
+  appId: "1:713344693424:web:507ed1397c7c62eb7fb775"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
